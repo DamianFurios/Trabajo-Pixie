dec392bae4349d6d41da4e7b289b08dcadd53da3
DamianFurios <damianfurios@gmail.com>
2022-03-13 18:46:38 -0300

    tarea clase 2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Container } from 'pixi.js';
 import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
@@ -5,31 +6,84 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
+// Evento para reescalar la pantalla
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale  = Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) /2 );
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) /2 );
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+
+});
+window.dispatchEvent(new Event("resize"));
+
+
+
+
  // const myLoader = new Loader(); // una forma de agregar un loader
- Loader.shared.add({url: "jarro.jpg", name: "myJar"});
  Loader.shared.add({url: "gato.png", name: "gato"});
 
  Loader.shared.onComplete.add(()=>{
 	 
-	 const clampy: Sprite = Sprite.from("gato.png");
-	 const jarro: Sprite = Sprite.from("myJar");
+	 const gato: Sprite = Sprite.from("gato.png");
+	 const sword: Sprite = Sprite.from("espada.png");
+	 const catEnemy: Sprite = Sprite.from("CatEnemy.png");
+	 const halberd: Sprite = Sprite.from("halberd.png");
 	 
-	 console.log("Dimensones gato", clampy.width, clampy.height);
-	 console.log("Dimensiones jarro", jarro.width, jarro.height);
-	 // clampy.anchor.set(0.5);
+
+	 gato.scale.set(0.35,0.35);
+
+	 catEnemy.scale.set(-0.4,0.4);
+
+	 sword.scale.set(0.1,0.1);
+	 sword.position.set(160,0);
+
+	 halberd.scale.set(-0.3,0.2);
+	 halberd.position.x += 100;
+	 halberd.position.y += 100;
+	 halberd.angle = 70;
+	
+
 	 
-	 clampy.x = 0; 
-	 clampy.y = app.screen.height / 2;
-	 
-	 jarro.x = app.screen.width / 2; 
-	 jarro.y = app.screen.height / 2;
-	 
-	 app.stage.addChild(clampy); 
-	 app.stage.addChild(jarro); 
+	const CatWhithSword: Container = new Container();
+
+	CatWhithSword.scale.set (1.5,1.5);
+	CatWhithSword.position.set (300,300);
+
+	 CatWhithSword.addChild(gato); 
+	 CatWhithSword.addChild(sword); 
+
+	 const CatEnemyWithWeapon: Container = new Container();
+
+	 CatEnemyWithWeapon.addChild(catEnemy);
+	 CatEnemyWithWeapon.addChild(halberd);
+
+	 CatEnemyWithWeapon.position.set(900,300);
+
+
+
+	
+
+	app.stage.addChild(CatWhithSword);
+	app.stage.addChild(CatEnemyWithWeapon);
+	
 
  });
 
